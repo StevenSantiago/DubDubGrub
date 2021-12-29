412b539efca7841875bfa687b1246c51c22872d2
Steven Santiago <stevenalfred93@yahoo.com>
2021-12-28 21:57:34 -0800

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Steven Santiago on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
